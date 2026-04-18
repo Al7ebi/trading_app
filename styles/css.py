a6504fe64c245737aaf01ba02d1318a4c2d2675c
Al7ebi <107194320+Al7ebi@users.noreply.github.com>
2026-04-19 02:09:29 +0300

    Update css.py

--- styles/css.py
+++ styles/css.py
@@ -1,1 +1,11 @@
+def load_css():
+    import streamlit as st
 
+    st.markdown("""
+    <style>
+    body {
+        background-color: #0e1117;
+        color: white;
+    }
+    </style>
+    """, unsafe_allow_html=True)
