bef27b272a5cb010a4adcf5ee4d3945f48dffaad
Al7ebi <107194320+Al7ebi@users.noreply.github.com>
2026-04-19 02:33:57 +0300

    Update sidebar.py

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,1 +1,12 @@
 
+import streamlit as st
+
+def show_sidebar():
+    st.sidebar.title("📊 Menu")
+
+    page = st.sidebar.radio(
+        "Choose page",
+        ["Dashboard", "تحليل سهم", "Radar"]
+    )
+
+    return page
