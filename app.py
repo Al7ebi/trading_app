5f32d15cd0e7b2d697bcac2ff4718d4115705194
Al7ebi <107194320+Al7ebi@users.noreply.github.com>
2026-04-19 02:13:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
 from styles.css import load_css
+from components.sidebar import show_sidebar
 
 load_css()
 
+page = show_sidebar()
+
 st.title("منصة التداول 🚀")
-st.write("أول تشغيل للتطبيق")
+
+if page == "Dashboard":
+    st.write("Main page (الصفحة الرئيسية)")
+
+elif page == "تحليل سهم":
+    st.write("Stock analysis (تحليل سهم)")
+
+elif page == "Radar":
+    st.write("Radar page (صفحة الرادار)")
