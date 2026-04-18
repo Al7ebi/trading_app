068cbd591ec19cb4477b66e9ae687a2878cfa12f
Al7ebi <107194320+Al7ebi@users.noreply.github.com>
2026-04-19 02:35:46 +0300

    Update css.py

--- styles/css.py
+++ styles/css.py
@@ -1,11 +1,35 @@
-def load_css():
-    import streamlit as st
+import streamlit as st
 
+def load_css():
     st.markdown("""
-    <style>
-    body {
-        background-color: #0e1117;
-        color: white;
-    }
-    </style>
+        <style>
+        /* خلفية عامة */
+        body {
+            background-color: #0E1117;
+            color: white;
+        }
+
+        /* العنوان */
+        h1 {
+            text-align: center;
+            color: #00FFAA;
+        }
+
+        /* السايدبار */
+        section[data-testid="stSidebar"] {
+            background-color: #111827;
+        }
+
+        /* الأزرار */
+        .stButton>button {
+            background-color: #00FFAA;
+            color: black;
+            border-radius: 10px;
+        }
+
+        /* النصوص */
+        p {
+            font-size: 18px;
+        }
+        </style>
     """, unsafe_allow_html=True)
